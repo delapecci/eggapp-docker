f04609907f830bdb3ac22bd0f54e1957628c53af
Li Chong <delapecci@gmail.com>
2018-09-10 18:33:38 +0800

    更新npm安装

--- eggapp.sh
+++ eggapp.sh
@@ -16,10 +16,10 @@ APP_NAME=${APP_NAME:-egg-app}
 # DATABASE_PORT=${MONGO_PORT:-27017}
 # DATABASE_NAME=${MONGO_DB:-strapi}
 
-if [ ! -d "$APP_NAME/node_modules" ]
-then
-  npm i --registry=https://registry.npm.taobao.org --prefix ./$APP_NAME
-fi
+# if [ ! -d "$APP_NAME/node_modules" ]
+# then
+npm i --registry=https://registry.npm.taobao.org --prefix ./$APP_NAME
+# fi
 
 cd $APP_NAME
 npm start
